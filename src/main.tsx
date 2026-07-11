a9ff774e5163d286b07b07751fda47600d9cb7ff
oksana_martyniuk <mjlinkoln@gmail.com>
2026-07-11 23:53:05 +0300

    fix modern-normalize error

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./components/App/App";
-import "modern-normalize"; // Уніфікація стилів у різних браузерах
+import "modern-normalize/modern-normalize.css";
 import "./index.css";
 
 // Створюємо клієнт для керування кешем запитів
